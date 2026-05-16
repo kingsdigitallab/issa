0dbce580048437c0e123323c313c9fe09aa9aeba
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-05-16 23:27:22 +0100

    feat(qwen3x): new experiments with video QA using Qwen3x

---
+++ experiments/qwen3x/experiments.md
@@ -0,0 +1,93 @@
+---
+* TIME = 2026-05-16T23:23:07.841941
+* VIDEO = DVC43998.mp4
+* VRAM used: 22.69 GB out of 24.0 GB
+* REQUEST =
+
+```json
+{
+  "model": "Qwen/Qwen3.5-2B",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "video_url",
+          "video_url": {
+            "url": "file:///home/gnoel/src/prj/issa/experiments/qwen3x/DVC43998.mp4"
+          }
+        },
+        {
+          "type": "text",
+          "text": "Summarize the video content in one sentence."
+        }
+      ]
+    }
+  ],
+  "max_tokens": 5120,
+  "temperature": 0.7,
+  "top_p": 0.8,
+  "presence_penalty": 1.5,
+  "extra_body": {
+    "top_k": 20,
+    "mm_processor_kwargs": {
+      "fps": 2,
+      "do_sample_frames": true
+    }
+  }
+}
+```
+
+* RESPONSE =
+
+```json
+{
+  "id": "chatcmpl-a7c1be45a117455d",
+  "choices": [
+    {
+      "finish_reason": "stop",
+      "index": 0,
+      "logprobs": null,
+      "message": {
+        "content": null,
+        "refusal": null,
+        "role": "assistant",
+        "annotations": null,
+        "audio": null,
+        "function_call": null,
+        "tool_calls": [],
+        "reasoning": "The video is a compilation of various news segments, interviews, and footage from different locations, featuring diverse topics such as sports, community events, and urban development."
+      },
+      "stop_reason": null,
+      "token_ids": null,
+      "routed_experts": null
+    }
+  ],
+  "created": 1778970228,
+  "model": "Qwen/Qwen3.5-2B",
+  "object": "chat.completion",
+  "service_tier": null,
+  "system_fingerprint": "vllm-0.21.0-2c744646",
+  "usage": {
+    "completion_tokens": 33,
+    "prompt_tokens": 14571,
+    "total_tokens": 14604,
+    "completion_tokens_details": null,
+    "prompt_tokens_details": null
+  },
+  "prompt_routed_experts": null,
+  "prompt_logprobs": null,
+  "prompt_token_ids": null,
+  "prompt_text": null,
+  "kv_transfer_params": null
+}
+```
+
+* VRAM used: 22.69 GB out of 24.0 GB
+* TIME = 2026-05-16T23:23:48.683761
+* DURATION = 00:00:40
+
+
+---
+
+
---
+++ experiments/qwen3x/vqa.py
@@ -0,0 +1,157 @@
+from openai import OpenAI
+import json
+from datetime import datetime
+import subprocess
+
+VIDEO_FILENAME = 'DVC43998.mp4'
+
+def format_time(delta):
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"    
+
+def print_dict(d):
+    print('')
+    print('```json')
+    print(json.dumps(d, indent=2))
+    print('```')
+    print('')
+    
+def print_system_vram_usage():
+    used, total = get_system_vram_used()
+    print(f'* VRAM used: {used:.2f} GB out of {total:.1f} GB')
+    
+def get_system_vram_used():
+    # Query GPU memory usage in MiB
+    result = subprocess.run(
+        ["nvidia-smi", "--query-gpu=memory.used,memory.total", "--format=csv,noheader,nounits"],
+        capture_output=True, text=True
+    )
+    
+    # Parse the output (e.g., "1024, 8192")
+    used_mib, total_mib = map(int, result.stdout.strip().split(", "))
+    
+    # Convert MiB to GiB
+#     used_gib = used_mib / 1024
+#     total_gib = total_mib / 1024
+    
+    return [used_mib / 1024, total_mib / 1024]
+    
+    # print(f"VRAM used: {used_gib:.2f} GB out of {total_gib:.1f} GB")
+
+    
+# Configured by environment variables
+client = OpenAI(
+    api_key="whatever",
+    base_url="http://localhost:8000/v1"    
+)
+
+print('---')
+print(f'* TIME = {datetime.now().isoformat()}')
+print(f'* VIDEO = {VIDEO_FILENAME}')
+print_system_vram_usage()
+t0 = datetime.now()
+
+
+if 0:
+
+    messages = [
+        {"role": "user", "content": "Just say Hello world in reverse. Do not output reasoning. Only output the final answer."},
+    ]
+    
+    print('PROMPT =')
+    print(json.dumps(messages, indent=2))
+    
+    res = client.chat.completions.create(
+        model="Qwen/Qwen3.5-2B",
+        messages=messages,
+        max_tokens=32768,
+        temperature=1.0,
+        top_p=1.0,
+        presence_penalty=2.0,
+        # reasoning_effort="none",
+        extra_body={
+            "top_k": 20,
+            "enable_thinking": False
+        }, 
+    )
+    # print("Chat response:", chat_response)
+
+if 1:
+    '''
+    vllm serve Qwen/Qwen3.5-2B --port 8000 --tensor-parallel-size 1 --max-model-len 262144 --reasoning-parser qwen3 --allowed-local-media-path /home/gnoel/src/prj/issa/experiments/ --media-io-kwargs '{"video": {"num_frames": -1}}'
+    '''
+        
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "video_url",
+                    "video_url": {
+                        "url": f"file:///home/gnoel/src/prj/issa/experiments/qwen3x/{VIDEO_FILENAME}"
+                    }
+                },
+                {
+                    "type": "text",
+                    "text": "Summarize the video content in one sentence."
+                }
+            ]
+        }
+    ]
+    
+#     print('PROMPT =')
+#     print(json.dumps(messages, indent=2))
+        
+    # When vLLM is launched with `--media-io-kwargs '{"video": {"num_frames": -1}}'`,
+    # video frame sampling can be configured via `extra_body` (e.g., by setting `fps`).
+    # This feature is currently supported only in vLLM.
+    #
+    # By default, `fps=2` and `do_sample_frames=True`.
+    # With `do_sample_frames=True`, you can customize the `fps` value to set your desired video sampling rate.
+    
+    options = {
+        'model': "Qwen/Qwen3.5-2B",
+        'messages': messages,
+        'max_tokens': 5*1024,
+        'temperature': 0.7,
+        'top_p': 0.8,
+        'presence_penalty': 1.5,
+        'extra_body': {
+            "top_k": 20,
+            "mm_processor_kwargs": {"fps": 2, "do_sample_frames": True},    
+        }
+    }
+
+    print('* REQUEST =')
+    print_dict(options)
+    
+    res = client.chat.completions.create(**options)
+#         model="Qwen/Qwen3.5-2B",
+#         messages=messages,
+#         max_tokens=5*1024,
+#         temperature=0.7,
+#         top_p=0.8,
+#         presence_penalty=1.5,
+#         extra_body={
+#             "top_k": 20,
+#             "mm_processor_kwargs": {"fps": 2, "do_sample_frames": True},
+#         }, 
+#     )
+
+t1 = datetime.now()
+duration = t1 - t0
+    
+print('* RESPONSE =')
+print_dict(json.loads(res.model_dump_json()))
+
+print_system_vram_usage()
+
+print(f'* TIME = {datetime.now().isoformat()}')
+
+if duration:
+    print(f'* DURATION = {format_time(duration)}')
+
+print('\n')
+print('-' * 3)
+print('\n')
