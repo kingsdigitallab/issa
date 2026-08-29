01600a716b134a875d06548f9d9bd0b25600bd59
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-08-29 16:44:51 +0100

    feat(ws1): added bash script to launch FS anser_videos_vlm

---
+++ workshops/ws1/answer_videos_vlm.bash
@@ -0,0 +1,12 @@
+cd /scratch/users/k1217897/prj/framesense/ 
+
+ANSWER_VIDEOS_VLM_MAX_TOKENS="1k" \
+ANSWER_VIDEOS_VLM_SEED=43 \
+ANSWER_VIDEOS_VLM_REASONING_EFFORT="none" \
+ANSWER_VIDEOS_VLM_MODEL=RedHatAI/Qwen3.8-27B-INT4 \
+ANSWER_VIDEOS_VLM_API_BASE=http://localhost:30000/v1 \
+ANSWER_VIDEOS_VLM_FILTER_QUESTIONS=programs_3xinc-35-27B-v12k-think-s43 \
+FRAMESENSE_DEBUG=1 \
+FRAMESENSE_COLLECTIONS=/scratch/prj/dh_issa/issa/workshops/ws1/collections.json \
+./venv/bin/python framesense.py answer_videos_vlm -r 
+
