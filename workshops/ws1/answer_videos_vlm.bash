3aea1a46f4c88e01309ca1c74ddd96a4a219c921
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-08-31 13:59:17 +0100

    chore(ws1): added fps param to framesense launch script

--- workshops/ws1/answer_videos_vlm.bash
+++ workshops/ws1/answer_videos_vlm.bash
@@ -1,13 +1,14 @@
-cd /scratch/users/k1217897/prj/framesense/ 
+cd framesense
 
+ANSWER_VIDEOS_VLM_MODEL=RedHatAI/Qwen3.8-27B-INT4 \
 ANSWER_VIDEOS_VLM_MAX_TOKENS="30k" \
 ANSWER_VIDEOS_VLM_VIDEO_TOKENS="64k" \
-ANSWER_VIDEOS_VLM_SEED="43" \
 ANSWER_VIDEOS_VLM_REASONING_EFFORT="xhigh" \
-ANSWER_VIDEOS_VLM_MODEL=RedHatAI/Qwen3.8-27B-INT4 \
-ANSWER_VIDEOS_VLM_API_BASE=http://localhost:30000/v1 \
-ANSWER_VIDEOS_VLM_FILTER_QUESTIONS=programs_3xinc-35-27B-v12k-think-s43 \
+ANSWER_VIDEOS_VLM_MAX_FPS="1" \
+ANSWER_VIDEOS_VLM_SEED="43" \
+ANSWER_VIDEOS_VLM_API_BASE="http://localhost:30000/v1" \
+ANSWER_VIDEOS_VLM_FILTER_QUESTIONS="programs_3xinc-35-27B-v12k-think-s43" \
 FRAMESENSE_DEBUG=1 \
-FRAMESENSE_COLLECTIONS=/scratch/prj/dh_issa/issa/workshops/ws1/collections.json \
+FRAMESENSE_COLLECTIONS="/scratch/prj/dh_issa/issa/workshops/ws1/collections.json" \
 ./venv/bin/python framesense.py answer_videos_vlm -r -f 234
 
