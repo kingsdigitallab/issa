e2809490f936d45355ac6ed104fe890ed7a8fe86
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-09-12 14:30:07 +0100

    chore(ws1): added a sample100 filter to the collection

--- workshops/ws1/answer_sep_vlm_batches.bash
+++ workshops/ws1/answer_sep_vlm_batches.bash
@@ -2,4 +2,4 @@ cd framesense
 
 FRAMESENSE_DEBUG=1 \
 FRAMESENSE_COLLECTIONS="/scratch/prj/dh_issa/issa/workshops/ws1/collections-batches.json" \
-./venv/bin/python framesense.py answer_separators_vlm -f "sample50" 
+./venv/bin/python framesense.py answer_separators_vlm -f "sample100" 
