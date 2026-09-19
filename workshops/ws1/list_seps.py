305a3cc0e1899b6f973221a3b141efacf57204b6
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-09-19 17:41:09 +0100

    feat(ws1): list_seps.py reports missing progs

--- workshops/ws1/list_seps.py
+++ workshops/ws1/list_seps.py
@@ -8,31 +8,37 @@ base_dir = Path('batches')
 # Find all files ending in .y (e.g., X.y) recursively
 files = base_dir.rglob('video_answers.json')
 
-print(f'{"file":40s} {"sep":3s} {"err":3s}')
-
 sep_types = Counter()
 
+missing_progs = 0
+
 files_count = 0
-for file_path in files:
+for file_path in sorted(list(files)):
     data = json.loads(file_path.read_text())['data']
     sep1 = data['sep1']
     seps = sep1['answer']
     errors = [c['error'] for c in sep1['stats']['chunks'] if c['error']]
+    
+    prog_paths = file_path.parent.rglob('*-prog.mp4')
+    progs_count = len(list(prog_paths))
 
     sep_types.update([s.get('tag', '') for s in seps])
     
     if len(seps) or errors:
         err_count_str = len(errors)
         err_count_str = str(err_count_str) if err_count_str else ''
-        print(f'{str(file_path):40s} {len(seps):3d} {err_count_str:3s}')
+        print(f'{str(file_path):40s}  {progs_count:3d} progs  {len(seps):3d} segs  {err_count_str:3s} errs')
+    
+    if progs_count == 0:
+        missing_progs += 1
 
     files_count += 1
 
 print()
 
-for type_count in sep_types.most_common(50):
+for type_count in sep_types.most_common(100):
     print(f'{type_count[1]} {type_count[0]}')
 
 print()
 
-print(f'{files_count} files;')
+print(f'{files_count} files; {missing_progs} missing progs')
